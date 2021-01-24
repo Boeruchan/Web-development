a64cdd1ff64553243b2be048b5d57e58de1c20f2
Boeruchan <chingman.fung@gmail.com>
2021-01-24 20:39:24 +0100

    start eindopdracht js
    html + css uitgevoerd. JS stappenplan gemaakt

---
+++ JS/Eindopdracht/eind_psuedo.js
@@ -0,0 +1,35 @@
+/*  stappenplan js
+- function onclick
+-   0 is tevens de startpunt
+-   onclick geeft button de waarde 0 t/m 9 en punt
+    button 0 = cijfer 0
+    button 1 = cijfer 1
+    etc
+
+    -------------------------------
+
+- operators
+button AC = refresh en begin in startwaarde 0
+button =/- = ?
+button % = delen door procent
+button / = delen
+button * keer
+button - = aftrekken
+button + = optellen
+utton = == totaal in span"totaal"
+
+----------------------------------
+
+Werking calculator
+voorbeeld
+button 1 - output totaal staat op scherm 1
++ - output totaal staat op scherm 1 +
+button 1 - output totaal staat op scherm 1 + 1
+button = (totaal)
+2 staat op scherm totaal.
+
+--------------------------------
+
+
+
+*/
