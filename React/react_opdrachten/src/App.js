14e3a4cac1cb8f4e8cb67cd47d03a82c41ea927e
Boeruchan <chingman.fung@gmail.com>
2021-02-24 17:11:53 +0100

    React week 12 dag 3
    todo app werkend gekregen

--- React/react_opdrachten/src/App.js
+++ React/react_opdrachten/src/App.js
@@ -1,18 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './App.css';
+import Todo from "./Todo/Todo";
+import Form from "./Todo/Form";
+import {nanoid} from "nanoid";
 
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
 
-function App() {
-  const [value, onChange] = useState(new Date());
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    })
+    setTasks(updatedTasks);
+    console.log(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map(task => (
+  <Todo
+  id={task.id}
+  name={task.name}
+  completed={task.completed}
+  key={task.id}
+  toggleTaskCompleted={toggleTaskCompleted}
+  deleteTask={deleteTask}
+  />
+  )
+  );
+
+  function addTask(name) {
+    const newTask = {id:"todo-" + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
 
   return (
-    <div className="App">
-      <h1>To-do-App</h1>
-       
-    
-       <Calendar />
-   </div>
+
+    <div className="todoapp stack-large">
+          <Calendar />
+      <Form addTask={addTask} />
+      <h1>TodoList</h1>
+ 
+    <ul
+      role="list"
+      className="todo-list stack-large stack-exception"
+      aria-labelledby="list-heading"
+    >
+      {taskList}
+      </ul>
+      </div>
   );
 }
 
--- React/react_opdrachten/src/Todo/Todo.js
+++ React/react_opdrachten/src/Todo/Todo.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-const ToDo = () => {
-return (
-    
-)
+
+export default function Todo(props) {
+    return (
+        <li className="todo stack-small">
+        <div className="c-cb">
+          <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+          delt
+          />
+          <label className="todo-label"
+          htmlFor={props.id}>
+            {props.name}
+          </label>
+          <button type="button" className="btn-delete"
+          onClick={() => props.deleteTask(props.id)}>
+          X <span className="visually-hidden">{props.name}</span>
+        </button>
+        <hr />
+      </div>
+      </li>  
+    );
 }
-
-export default 'Todo';
