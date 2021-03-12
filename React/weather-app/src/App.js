8dee25bd9e1a8456eb4ad573b0cf03417220e7a4
Boeruchan <chingman.fung@gmail.com>
2021-03-12 14:03:12 +0100

    react week 16-dag 5
    weatherapp part 1 af

--- React/weather-app/src/App.js
+++ React/weather-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import React, { useState } from "react";
 import './App.css';
 import WeatherFetch from './Components/WeatherFetch';
 
 function App() {
   return (
     <div className='App'>
-      <WeatherFetch />
+      <WeatherFetch city="Hawaii"/>
+      <WeatherFetch city="Zwijndrecht"/>
   </div>
 )
   }
--- React/weather-app/src/Components/WeatherFetch.js
+++ React/weather-app/src/Components/WeatherFetch.js
@@ -1,43 +1,57 @@
 import React,  { useState, useEffect } from 'react';
-// import axios from 'axios'; 
 
-function WeatherFetch() {
+function WeatherFetch({city}) {
     const [posts, setPosts] = useState([]);
-
-    // useEffect(() => {
-    //     axios.get(`https://api.openweathermap.org/data/2.5/weather?id=2172797&n&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`)
-    //     .then(res => {
-    //         console.log(res)
-    //         setPosts(res.data)
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
-    // }, [])
-
-    
+ 
     useEffect(() => {
         const getDataFromServer = async()=>{
-            let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=2172797&n&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`)
+            let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
              let data = await req.json();
              console.log(data);
-            setPosts(data);
-         
+            setPosts(data);        
           }
         getDataFromServer()
  
     }, []);
 
-    return (
-        <div>
-            {posts.name}<br />
-            {posts.main.temp}
-            <ul>
-                {/* {posts.map(post => (
-                    <li key={post.weather}>{post.weather.main}</li>)
-                )} */}
-            </ul>
-            test
+    const card = {
+        backgroundColor: 'lightblue',
+        height: '250px',
+        width: '200px',
+        border: '2px solid black',
+        borderRadius: '25px',
+        padding: '20px',
+        textAlign: 'center',
+        margin: '20px'
+    }
+
+    const cardText = {
+        display: 'column',
+        backgroundColor: 'white',
+        borderRadius:'15px',
+        padding: '10px',
+        fontFamily: 'Ariel'
+    }
+
+    return(
+        
+        <div style={card}>
+            <h1>{posts.name}</h1>
+           <img src={`http://openweathermap.org/img/wn/${posts.weather[0].icon}@2x.png`} alt='' />
+           <b>{posts.main.temp}</b>
+           <br /><br />
+            <table style={cardText}>
+                <tr>
+                <td>min temp</td> 
+                <td>max temp</td> 
+                <td>humidity</td> 
+                </tr>
+                <tr>
+                    <td>{posts.main.temp_min}</td>
+                    <td>{posts.main.temp_max}</td>
+                    <td>{posts.main.humidity}</td> 
+                </tr>
+            </table>
         </div>
     )
 }
