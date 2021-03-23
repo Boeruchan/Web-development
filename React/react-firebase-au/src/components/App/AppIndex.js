454ad48127c4d45e13dad012376cfca0c5514f96
Boeruchan <chingman.fung@gmail.com>
2021-03-23 20:24:55 +0100

    react week 17 firebase
    start firebase

---
+++ React/react-firebase-au/src/components/App/AppIndex.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as ROUTES from '../../Constants/Routes';
+import Navigation from '../Navigation/NavIndex';
+
+import LandingPage from '../Landing/LandingIndex';
+import SignUpPage from '../SignUp/SignUpIndex';
+import SignInPage from '../SignIn/SignInIndex';
+import PasswordForgetPage from '../PasswordForget/PwForgetIndex';
+import Home from '../Home/HomeIndex';
+import AccountPage from '../Account/AccountIndex';
+import AdminPage from '../Admin/AdminIndex';
+
+
+
+const App = () => (
+  <div>
+     <Router>
+       <div>
+    <Navigation />
+    <hr />
+    <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={Home} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
+  </div>
+);
+ 
+export default App;
--- React/weather-app/src/App.js
+++ React/weather-app/src/App.js
@@ -4,27 +4,40 @@ import WeatherFetch from './Components/WeatherFetch';
 import WeatherDetail from './Components/WeatherDetail';
 
 function App() {
-  const [active, setActive] = useState(null)
+  const [toggle, setToggle ] = useState(null);
+  const [active, setActive] = useState(null);
+
+  const toggled = () => {
+    setToggle(!toggle);
+  }
+
+  console.log(toggled)
+
   return (
     <div className='App'>
-      <WeatherFetch city="Amsterdam"/>
-      <WeatherDetail
-      lat="52.374"
-      ion="4.8897"
+      <div className='city'>
+      <button onClick={toggled}>
+        <WeatherFetch city="Amsterdam"/>
+        </button>
+        <div className='detail'>
+          {toggle ? <WeatherDetail lat="52.375" ion="4.8897" active={active} setActive={setActive} /> : null}
+          </div>
+      </div>
+    
+
+{/* <WeatherDetail lat="52.374" ion="4.8897"
       active={active}
       setActive={setActive}
-      />
-{/*       
-        <WeatherDetail
-      lat="52.375"
-      ion="4.8897"
+      /> } */}
+
+{/* <WeatherDetail lat="52.374" ion="4.8897"
       active={active}
-      setActive={setActive}
-      />
-      <WeatherFetch city="Zwijndrecht"/>
-      */}
+      setActive={setActive} */}
+
+{/* <WeatherFetch city="Zwijndrecht"/> */}
+
   </div>
 )
-  }
+    }
 
 export default App;
